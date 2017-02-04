4ada02018b3ae785844b9daabaaff7161173bdc2
Thomas Russel Carrel <tcarrel@mail.csuchico.edu>
2017-02-03 18:12:05 -0800

    Made program remove some comments.

--- glsl_to_c.cpp
+++ glsl_to_c.cpp
@@ -299,8 +299,15 @@ int main( int argc, char* argv[] )
                     {
                         if( line != "" )
                         {
+                            // Nested for readability.
+                            // Strips whole-line c-style comments,
+                            // (Those beginning with // as the first two
+                            // characters on the line.
+                            if( line[0] != '/' && line[0] && line[1] )
+                            {
                             file_text += DQ + line + "\\n" + DQ + "\n";
                             length += line + "\n";
+                            }
                         }
                     }
 
